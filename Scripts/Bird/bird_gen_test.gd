cbf6e3939805016eccd16598452c234829bab463
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2023-11-05 18:41:56 +0000

    Begin work on the Bird Asset Generator
    Created a system message for the API
    Created a basic script to manipulate the image

--- Scripts/Bird/bird_gen_test.gd
+++ Scripts/Bird/bird_gen_test.gd
@@ -7,7 +7,9 @@ func _enter_tree():
 	pass
 
 # Called when the node enters the scene tree for the first time.
-#func _ready():
+func _ready():
+	var result: AuthTask = await Supabase.auth.sign_in("*", "*").completed
+	print(result)
 #	var halfway_x = (head_start.position.x + head_end.position.x)/2
 #	var halfway_y = (head_start.position.y + head_end.position.y)/2
 #	var halfway_point = Vector2(halfway_x, halfway_y)
@@ -34,7 +36,7 @@ func _enter_tree():
 var change = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if change:
 		$AnimatedSprite2D.play("Take-off")
 	else:

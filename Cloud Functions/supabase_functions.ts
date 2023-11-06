78833e6d13beba3f60355a429b7a9525e451e532
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2023-11-06 11:00:40 +0000

    Testing new system message and template

---
+++ Cloud Functions/supabase_functions.ts
@@ -0,0 +1,13 @@
+import {} from "./supabase_helper_functions.ts"
+import { OpenAI } from "https://esm.sh/openai@4.11.1";
+import { load } from "https://deno.land/std@0.202.0/dotenv/mod.ts";
+
+async function findSpecies(species_name: string) {
+
+}
+
+async function main() {
+    
+}
+
+main();

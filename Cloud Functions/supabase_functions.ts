6021f49a1e4975ecc155d6bf293b80ffaa48ce29
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2023-11-07 20:02:05 +0000

    Create the server function to check if the bird exists in the database

--- Cloud Functions/supabase_functions.ts
+++ Cloud Functions/supabase_functions.ts
@@ -1,13 +1,74 @@
 import {} from "./supabase_helper_functions.ts"
 import { OpenAI } from "https://esm.sh/openai@4.11.1";
 import { load } from "https://deno.land/std@0.202.0/dotenv/mod.ts";
+import { SupabaseClient, createClient } from 'https://esm.sh/@supabase/supabase-js@2.38.4'
 
-async function findSpecies(species_name: string) {
+// Temp for local testing
+const env = await load();
+const SUPABASE_SERVICE_ROLE_KEY = env["SUPABASE_SERVICE_ROLE_KEY"];
+const SUPABASE_URL = env["SUPABASE_URL"];
+const birdSpeciesTable = "BirdSpecies";
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+    'Content-Type': 'application/json'
+  };
 
+async function findSpecies(request: Request): Promise<Response> {
+    const requestUrl = new URL(request.url)
+    const speciesName = requestUrl.searchParams.get("species");
+    const nameCol = "birdName"
+    const supabaseAdminClient = createClient(
+        SUPABASE_URL,
+        SUPABASE_SERVICE_ROLE_KEY
+    );
+
+    if (speciesName == null) {
+        return new Response(JSON.stringify({ error: "Parameter 'species' is missing" }), {
+            headers: headers,
+            status: 400
+        });
+    }
+
+    const { data, error } = await supabaseAdminClient.from(birdSpeciesTable).select().eq(nameCol, speciesName);
+    
+    if (error != null) {
+        console.log(error)
+        return new Response(JSON.stringify({ error: error.message }), {
+            headers: headers,
+            status: 400
+        });
+    }
+
+    if (data.length == 0) {
+        // TODO Call the create bird function
+        await findWikiPage(speciesName);
+        return new Response(JSON.stringify({ message: "Creating new bird" }), {
+            headers: headers,
+            status: 200
+        });
+    }
+
+    return new Response(JSON.stringify({ data: data }), {
+        headers: headers,
+        status: 200
+    });
+}
+
+async function findWikiPage(species: string): Promise<Response> {
+    
+    
+    return new Response();
 }
 
 async function main() {
-    
+    const newRequest  = new Request("http://test.com/test?species=test", {
+        headers: headers
+    });
+    console.log(await (await findSpecies(newRequest)).json())
+
+    //const result = await findSpecies("tests", supabaseAdminClient);
+    //console.log(result)
 }
 
 main();
--- Cloud Functions/test.ts
+++ Cloud Functions/test.ts
@@ -1,4 +1,4 @@
-import { OpenAI } from "https://esm.sh/openai@4.11.1";
+import { OpenAI } from "npm:openai@4.16.1";
 import { load } from "https://deno.land/std@0.202.0/dotenv/mod.ts";
 
 const env = await load();
@@ -8,66 +8,59 @@ const openai = new OpenAI({
     apiKey: OPENAI_API_KEY,
 });
 
-function getCurrentWeather(location:string, units:string):string {
-    return JSON.stringify({
-        location: location,
-        temperature: 27,
-        unit: units,
-        forecast: ["sunny", "windy"]
-    });
-}
-
 async function main() {
-    const message:any[] = [{ role: 'user', content: 'What\'s the weather like in Boston?' }];
-    const functions = [
+  const response = await openai.chat.completions.create({
+    model: "gpt-4-vision-preview",
+    messages: [
         {
-            "name": "getCurrentWeather",
-            "description": "Get the current weather in a given location",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "location": {
-                        "type": "string",
-                        "description": "The city and state, e.g. San Francisco, CA",
-                    },
-                    "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
-                },
-                "required": ["location"],
-            },
-        }
-    ]
-
-    const chatCompletion = await openai.chat.completions.create({
-      messages: message,
-      functions: functions,
-      function_call: "auto",
-      model: 'gpt-3.5-turbo',
-    });
-  
-    const response = chatCompletion.choices[0].message;
-
-    if(response.function_call) {
-        const functionName:string = response.function_call.name as string;
-        if(functionName == "getCurrentWeather") {
-            const functionArgs = response.function_call.arguments;
-            const result = getCurrentWeather(functionArgs[0], functionArgs[1]);
-            message.push(response);
-            message.push({
-                role: "function",
-                name: functionName,
-                content: result
-            })
-
-            const secondResult = await openai.chat.completions.create({
-                messages: message,
-                model: 'gpt-3.5-turbo',
-            })
-            console.log(secondResult);
-        }
-    }
-    else {
-        console.log(response.content)
-    }
+            role: "system",
+            content: [
+                { type: "text", text: `
+                You will given a text description of a bird and a picture of template. Your role is to extract colour information from the description of the various parts of the bird then store the rgb value as an array in the to corresponding body part in the json structure defined here which corresponds with given image"
+                {
+                    "Crown": [255,126,0],
+                    "UpperSupercilium": [133, 98, 61],
+                    "MiddleSupercilium": [255,163,177],
+                    "LowerSupercilium": [79, 36, 69],
+                    "Nape": [237,28,36],
+                    "Nostril": [255,194,14],
+                    "UpperBeak": [168,230,29],
+                    "LowerBeak": [34,177,76],
+                    "Auriculars": [255,242,0],
+                    "Throat": [47,54,153],
+                    "Breast": [153,217,234],
+                    "Wing": [255,249,189],
+                    "Side": [229,170,122],
+                    "Belly": [84,109,142], 
+                    "Thigh": [156,90,60],
+                    "Leg": [0,183,239],
+                    "UndertailCoverts": [245,228,156],
+                    "Tail": [211,249,188],
+                    "Rump": [180,180,180],
+                    "Flanks": [111, 49, 152]
+                }                
+". If the description does not define the colour you can use the colour of the closest body part instead within reason. The legs are a different colour from the feathers, if not defined just use the common leg colour of the bird type. Don't use max or min values like [255,255,255] or [0,0,0] etc. Also note that synonyms may be used in the description. Prefer males. No null values. Just the json.
+                `}
+            ]
+        },
+      {
+        role: "user",
+        content: [
+          { type: "text", text: "The Eurasian blue tit is usually 12 cm (4.7 in), long with a wingspan of 18 cm (7.1 in) for both sexes, and weighs about 11 g (0.39 oz).[10] A typical Eurasian blue tit has an azure-blue crown and dark blue line passing through the eye, and encircling the white cheeks to the chin, giving the bird a very distinctive appearance. The forehead and a bar on the wing are white. The nape, wings and tail are blue and the back is yellowish green. The underparts are mostly sulphur-yellow with a dark line down the abdomen—the yellowness is indicative of the number of yellowy-green caterpillars eaten, due to high levels of carotene pigments in the diet.[11] The bill is black, the legs bluish grey, and the irides dark brown. The sexes are similar and often indistinguishable to human eyes, but under ultraviolet light, males have a brighter blue crown.[12] Young blue tits are noticeably more yellow. " },
+          {
+            type: "image_url",
+            image_url:
+              "https://sjiikegnbgahukdwklux.supabase.co/storage/v1/object/sign/BirdAssets/Templates/Chickadees/chickadees-sprite-sheet.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJCaXJkQXNzZXRzL1RlbXBsYXRlcy9DaGlja2FkZWVzL2NoaWNrYWRlZXMtc3ByaXRlLXNoZWV0LnBuZyIsImlhdCI6MTY5OTMwNTQ0MSwiZXhwIjoxNjk5OTEwMjQxfQ.DqC3gbFYAdniVVk0UO8RnaYF-pslhHaM3hnG4CR2suY&t=2023-11-06T21%3A17%3A21.434Z",
+          },
+        ],
+      },
+    ],
+    max_tokens: 500
+  });
+  console.log(response.choices[0]);
+  let write = JSON.stringify(response.choices[0]);
+  await Deno.writeTextFile("result.json", write);
 }
+
   
   main();

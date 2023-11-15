e15f72c14ce845c9ecb08eb0518236ea5e8d4ba8
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2023-11-15 12:56:01 +0000

    Create the species resource

---
+++ Scripts/Bird/bird_species_info.gd
@@ -0,0 +1,13 @@
+extends Resource
+
+class_name BirdSpecies
+
+var bird_animations: SpriteFrames
+var bird_range: float
+var bird_stamina: float
+var bird_take_off_cost: float
+var bird_ground_cost: float
+var bird_flight_cost: float
+var bird_max_stamina: float 
+
+var bird_traits: Array[String]

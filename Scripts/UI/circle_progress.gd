b7ed1cfa7e2fe38fc7e2c2b7d67080a3506b2f7c
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2024-02-02 20:49:22 +0000

    Renaming files

--- Scripts/UI/circle_progress.gd
+++ Scripts/UI/circle_progress.gd
@@ -3,12 +3,12 @@ extends TextureProgressBar
 
 var current_value = 0
 
-func _process(delta):
+func _process(delta)->void:
 	current_value = (current_value + 5) % 360
 	radial_initial_angle = current_value
 
-func hide_loading():
+func hide_loading()->void:
 	hide()
 
-func show_loading():
+func show_loading()->void:
 	show()
--- Scripts/UI/zoom_in_button.gd
+++ Scripts/UI/zoom_in_button.gd
@@ -1,6 +1,6 @@
-extends TextureButton
+extends Button
 
 
-func _on_pressed():
+func _on_pressed()->void:
 	get_tree().call_group("PlayerCamera", "zoom_in")
 

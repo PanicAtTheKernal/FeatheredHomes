322652ba73cad871e4990c69fa74f8644555f9e3
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2024-02-25 00:36:55 +0000

    Fix wander and avoidance

--- Scripts/Bird/Boids/wander.gd
+++ Scripts/Bird/Boids/wander.gd
@@ -6,15 +6,23 @@ var bird: Bird
 var noise: FastNoiseLite
 
 var frequency = 0.3
-var radius = 3
+var radius = 10
 var theta = 0
 # Wander angle
-var amplitude = 80
-var distance = 10
+var amplitude = 160
+var distance = 20
+var feeler_distance: float
 var target: Vector2
 var world_target: Vector2
+var current_angle: float = 0
 var w_angle: float = 20
 var force: Vector2
+var jitter = 3.0
+#Debug
+var debug_feeler: DebugGizmos.DebugLine
+var debug_displacement: DebugGizmos.DebugLine
+var debug_velocity: DebugGizmos.DebugLine
+
 
 func _init(parent_bird: Bird, node_name:String="Wander") -> void:
 	super(node_name)
@@ -25,27 +33,29 @@ func _init(parent_bird: Bird, node_name:String="Wander") -> void:
 	noise.set_frequency(0.01)
 	noise.set_fractal_lacunarity(2)
 	noise.set_fractal_gain(0.5)
+	feeler_distance = distance + 10
 
 func run()->void:
 	force = Vector2.ZERO
 	force += _wander()
-	force -= _avoidance()
+	#force += _seek(Vector2(0,0))
+	force += _avoidance()
+	if DebugGizmos.enabled:
+		debug_velocity.draw([bird.global_position, (bird.global_position+force)])
 	var acceleration = force/ bird.mass
 	bird.velocity = bird.velocity + acceleration * get_physics_process_delta_time()
 	bird.move_and_slide()
-	super.success()	
-	#var bird_at_target = bird.at_target()
-	#if not bird_at_target and bird.nav_agent.is_target_reachable():
-		#bird.move_and_slide()
-		#super.running()	
-	#elif bird_at_target:
-		#bird.target_reached = true
-		#super.success()
-	#elif not bird.nav_agent.is_target_reachable():
-		#super.fail()
+	super.success()
 	
 func start()->void:
 	super.start()
+	if DebugGizmos.enabled:
+		debug_feeler = DebugGizmos.DebugLine.new(Color.ORANGE)
+		debug_displacement = DebugGizmos.DebugLine.new(Color.RED)
+		debug_velocity = DebugGizmos.DebugLine.new(Color.GREEN)
+		add_child(debug_feeler)
+		add_child(debug_displacement)	
+		add_child(debug_velocity)
 
 func _seek(target: Vector2)->Vector2:
 	var to_target = (target - bird.global_position).normalized()
@@ -55,34 +65,34 @@ func _seek(target: Vector2)->Vector2:
 func _wander()->Vector2:
 	var noise_value = noise.get_noise_1d(theta)
 	var angle = deg_to_rad(noise_value * amplitude)
-	#var angle = deg_to_rad(randi_range(-w_angle, w_angle))
-	# There is only 1 rotation in a 2D space
-	var rotation = bird.global_rotation
-
-	#var displacment: Vector2 = w_angle * Vector2(randf(), randf()) * get_physics_process_delta_time()
-	#target += displacment
-	#target = target.limit_length(radius)
-	#
-	#target.x = sin(angle)
-	#target.y = cos(angle)
-	#
-	#target.rotated(angle)
-	#
-	#target *= radius
-	
-	#target = Vector2(radius*angle, radius*angle)
-	
-	var local_target = target * distance
-	world_target = (bird.global_position + Vector2(10,10)).rotated(angle)
+	# Get new direction but it's not on the circle circumference
+	target += Vector2(randf_range(-1.0,1.0), randf_range(-1.0,1.0)).rotated(angle)
+	# Need to place the target back onto the circumference, normalise it so target is less than 1
+	# and then times it by the radius of the circle
+	target = target.normalized() * radius
+	# This where you move the target to be infront of the bird, moving it forward with the new vector
+	var local_target = Vector2.ZERO	
+	# There is no forward vector in a 2D space
+	# The bird can go up, down, left or right, so the target becomes the direction
+	local_target = target.normalized() * distance
+	world_target = (bird.global_position + local_target)
+	if DebugGizmos.enabled:
+		debug_displacement.draw([bird.global_position, world_target])
 	theta += frequency * get_physics_process_delta_time() * PI * 2.0
 	return _seek(world_target)
+	#return Vector2.ZERO
 	
 func _avoidance()->Vector2:
 	var local_force = Vector2.ZERO
 	var state_space = bird.get_world_2d().direct_space_state
-	# 100 = ray len
-	var query = state_space.intersect_ray(PhysicsRayQueryParameters2D.create(bird.global_position, bird.global_position + bird.velocity.normalized() * 100, bird.collision_mask))
-	if not query.is_empty(): 
-		local_force += -query["normal"] * 1000
-		#force -= bird.velocity * 50
+	var query = PhysicsRayQueryParameters2D.create(bird.global_position, bird.global_position + (bird.velocity.normalized() * feeler_distance), bird.collision_mask)
+	var result = state_space.intersect_ray(query)
+	if DebugGizmos.enabled:
+		debug_feeler.draw([bird.global_position, bird.global_position + (bird.velocity.normalized() * feeler_distance)])
+	if not result.is_empty(): 
+		var to_bird = bird.global_position - result.position
+		var force_magnatiude = (feeler_distance - to_bird.length()) * (distance)
+		#Invert the wander direction to force the bird to turn around and not be fighting with wander forcing the bird in the other direction
+		target = -target
+		local_force += result.normal * force_magnatiude
 	return local_force
